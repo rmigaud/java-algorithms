db28673976e0c6d4a62a5e895917ff99af5505d6
Ryan Migaud <rmigaud@asu.edu>
2023-02-03 20:28:32 -0800

    checkstyle refactoring

--- algorithms/src/main/java/j/algorithms/AlgorithmsApplication.java
+++ algorithms/src/main/java/j/algorithms/AlgorithmsApplication.java
@@ -3,11 +3,13 @@ package j.algorithms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ Entry Point for running the AlgorithmsApplication.
+ */
 @SpringBootApplication
 public class AlgorithmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AlgorithmsApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(AlgorithmsApplication.class, args);
+  }
 }
--- algorithms/src/test/java/j/algorithms/AlgorithmsApplicationTests.java
+++ algorithms/src/test/java/j/algorithms/AlgorithmsApplicationTests.java
@@ -3,10 +3,14 @@ package j.algorithms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class AlgorithmsApplicationTests {
 
     @Test
     void contextLoads() {
+      final String contextLoaded = "hello-spring";
+      assertThat(contextLoaded).isNotNull();
     }
 }
